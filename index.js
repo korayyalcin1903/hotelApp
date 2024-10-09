b1e205f45983cfab4a031b9d696b0a1cce9fc268
Koray Yalçın <korayyalcin1903@gmail.com>
2024-10-09 21:28:12 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+const path = require('path')
+
+app.set('view engine', 'ejs')
+
+const homeRoutes = require('./routers/home')
+
+
+app.use(express.urlencoded({extended: true}))
+app.use('/', homeRoutes)
+
+app.use('/libs', express.static(path.join(__dirname, 'node_modules')))
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.listen(3000, () => {
+    console.log("listening on port 3000")
+})
