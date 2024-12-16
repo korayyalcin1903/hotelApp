347603ed34c432a2ec8ebc4260629f24033a4738
korayyalcin1903 <korayyalcin1903@gmail.com>
2024-12-16 22:42:48 +0300

    veritabanı biligleri güncellendi

--- chatbot/config.py
+++ chatbot/config.py
@@ -8,5 +8,5 @@ app.config['MYSQL_USER'] = 'avnadmin'
 app.config['MYSQL_PASSWORD'] = 'AVNS_DHHTqi3Ep9EJ6InEeLQ'  
 app.config['MYSQL_DB'] = 'defaultdb'     
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'  
-app.config['MYSQL_POOL_SIZE'] = 10  
+app.config['MYSQL_POOL_SIZE'] = 10 
 app.config['MYSQL_POOL_RECYCLE'] = 300  
